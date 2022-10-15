cb57173476658cc9ffc81f6ec4789a14c2d5ca4f
harryschreiber <hvs4th@gmail.com>
2022-10-15 13:29:14 -0400

    Preliminary Lat, Lon setup

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  locationArray: string[] = Array<string>()
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.setData()
+    },1000)
+  }
+
+  setData(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+
+      this.locationArray.push(`Lat: ${lat}, Lon: ${lon}`)
+    })
   }
 
 }
